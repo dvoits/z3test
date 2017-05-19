f97878a1a227390d55617c8770be2325e88a14d4
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2017-05-19 12:03:28 +0300

    Replaced 2 console app project to be used within batch with a single one.

---
+++ src/AzurePerformanceTest/AzureWorker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Measurement;
+
+namespace AzureWorker
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                //TODO: Proper help
+                Console.WriteLine("Not enough arguments.");
+                return 1;
+            }
+
+            var subArgs = args.Skip(1).ToArray();
+            switch (args[0])
+            {
+                case "--add-tasks":
+                    AddTasks(subArgs).Wait();
+                    return 0;
+                case "--measure":
+                    Measure(subArgs).Wait();
+                    return 0;
+                case "--reference-run":
+                    RunReference(subArgs).Wait();
+                    return 0;
+                default:
+                    Console.WriteLine("Incorrect first parameter.");
+                    return 1;
+            }
+        }
+
+        static async Task AddTasks(string[] args)
+        {
+
+        }
+
+        static async Task Measure(string[] args)
+        {
+
+        }
+
+        static async Task RunReference(string[] args)
+        {
+
+        }
+    }
+}
--- src/AzurePerformanceTest/AzureWorker/Properties/AssemblyInfo.cs
+++ src/AzurePerformanceTest/AzureWorker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AzureBatchJobStarter")]
+[assembly: AssemblyTitle("AzureWorker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AzureBatchJobStarter")]
+[assembly: AssemblyProduct("AzureWorker")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d41bc32-fa82-4b8e-aa9b-df906036c2c0")]
+[assembly: Guid("89dbb143-52e0-4c0a-a637-911fe977161a")]
 
 // Version information for an assembly consists of the following four values:
 //
